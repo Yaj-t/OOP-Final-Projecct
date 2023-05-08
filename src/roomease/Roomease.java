4de0d4719b441bc6d37a61f86d1c0dfd6e0449ea
CarlVictorV <19104214@usc.edu.ph>
2023-05-08 13:02:00 +0800

    Update Roomease.java

--- src/roomease/Roomease.java
+++ src/roomease/Roomease.java
@@ -1,28 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package roomease;
-
-import database.Connect;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- *
- * @author Predator
- */
 public class Roomease {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         LoginPage login = new LoginPage();
         login.setVisible(true);
-//        new Home().setVisible(true);
-        
     }
-    
 }
