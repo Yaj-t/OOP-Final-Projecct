3fdbd74a8b2b76ce75cc0af60ab2716b6a10fe9f
CarlVictorV <19104214@usc.edu.ph>
2023-05-08 14:33:04 +0800

    a

---
+++ src/database/RentalDAO.java
@@ -0,0 +1,12 @@
+package database;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import util.Rental;
+
+public class RentalDAO {
+    
+
+}
--- src/util/Rental.java
+++ src/util/Rental.java
@@ -1,29 +1,21 @@
 package util;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class Rental {
-    
 
-    // rental_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    // tenant_id int(11) NOT NULL,
-    // room_id int(11) NOT NULL,
-    // check_in_date datetime NOT NULL,
-    // check_out_date datetime NOT NULL,
-    // total_amount decimal(10,2) NOT NULL
-    
     //Attributes
     private int rental_id;
     private int tenant_id;
     private int room_id;
-    private LocalDateTime check_in_date;
-    private LocalDateTime check_out_date;
+    private LocalDate check_in_date;
+    private LocalDate check_out_date;
     private double total_amount;
 
     //Constructor
     public Rental() {
     }
 
-    public Rental(int rental_id, int tenant_id, int room_id, LocalDateTime check_in_date, LocalDateTime check_out_date, double total_amount) {
+    public Rental(int rental_id, int tenant_id, int room_id, LocalDate check_in_date, LocalDate check_out_date, double total_amount) {
         this.rental_id = rental_id;
         this.tenant_id = tenant_id;
         this.room_id = room_id;
@@ -57,19 +49,19 @@ public class Rental {
         this.room_id = room_id;
     }
 
-    public LocalDateTime getCheck_in_date() {
+    public LocalDate getCheck_in_date() {
         return this.check_in_date;
     }
 
-    public void setCheck_in_date(LocalDateTime check_in_date) {
+    public void setCheck_in_date(LocalDate check_in_date) {
         this.check_in_date = check_in_date;
     }
 
-    public LocalDateTime getCheck_out_date() {
+    public LocalDate getCheck_out_date() {
         return this.check_out_date;
     }
 
-    public void setCheck_out_date(LocalDateTime check_out_date) {
+    public void setCheck_out_date(LocalDate check_out_date) {
         this.check_out_date = check_out_date;
     }
 
