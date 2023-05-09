0095ef0ab8905c2b82a6f196f1c23cb836d31523
CarlVictorV <19104214@usc.edu.ph>
2023-05-10 04:24:53 +0800

    Update EmployeeLoginLogs.java

--- src/util/EmployeeLoginLogs.java
+++ src/util/EmployeeLoginLogs.java
@@ -10,4 +10,8 @@ public class EmployeeLoginLogs extends LoginLogs {
     public EmployeeLoginLogs(int logId, int employeeId, LogType type, LocalDateTime logTime) {
         super(logId, employeeId, type, logTime);
     }
+
+    public EmployeeLoginLogs(int employeeId, LogType type) {
+        super(employeeId, type);
+    }
 }
