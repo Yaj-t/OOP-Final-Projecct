4979e3a4a646d860d26ded6d3d8b6a940fb1de05
CarlVictorV <19104214@usc.edu.ph>
2023-05-10 17:35:45 +0800

    Create WindowCloseHandler.java
    A easier way to handle close logout

---
+++ src/util/WindowCloseHandler.java
@@ -0,0 +1,68 @@
+package util;
+
+import enums.LogType;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+
+import javax.swing.JFrame;
+
+import database.AdminLogs;
+import database.EmployeeLogs;
+import user.Session;
+
+public class WindowCloseHandler {
+
+    /**
+     * Adds a window closing listener to the specified JFrame
+     * @param frame The JFrame to add the listener to
+     */
+    public static void addWindowClosingListener(JFrame frame) {
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                handleWindowClosing();
+            }
+        });
+    }
+
+    /**
+     * Handles the actions when the window is closing
+     */
+    private static void handleWindowClosing() {
+        // Check if the user is Admin or Employee by taking the user type from the session
+        if (Session.getCurrentUser().getType() == enums.UserType.ADMIN) {
+            createAdminLoginLog(Session.getCurrentUserId(), LogType.LOGOUT);
+        } else {
+            createEmployeeLoginLog(Session.getCurrentUserId(), LogType.LOGOUT);
+        }
+
+        
+        System.exit(0);
+    }
+
+    /**
+     * Creates an admin login log
+     */
+    private static void createAdminLoginLog(int userId, LogType logType) {
+        AdminLoginLogs log = new AdminLoginLogs(userId, logType);
+        try {
+            AdminLogs.createAdminLoginLog(log);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates an employee login log
+     */
+    private static void createEmployeeLoginLog(int userID, LogType logType)
+    {
+        EmployeeLoginLogs log = new EmployeeLoginLogs(userID, logType);
+        try {
+            EmployeeLogs.createEmployeeLoginLog(log);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
