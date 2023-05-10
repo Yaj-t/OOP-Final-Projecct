66df2a7c92dbab3459cd8d3f6bbfc2f433159912
T-jay Abunales <tjabunales@gmail.com>
2023-05-10 21:57:11 +0800

    documentation login and roomease

--- src/roomease/Roomease.java
+++ src/roomease/Roomease.java
@@ -1,4 +1,11 @@
+
+/**
+
+The Roomease class is the entry point for the application. It contains the main method
+which creates and displays the login page of the application.
+*/
 package roomease;
+
 public class Roomease {
     
     public static void main(String[] args) {
