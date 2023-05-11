cc369f5293a99166a49cfce1daa77cabde4394e5
T-jay Abunales <tjabunales@gmail.com>
2023-05-11 23:07:46 +0800

    another one

--- src/roomease/Roomease.java
+++ src/roomease/Roomease.java
@@ -1,10 +1,9 @@
+package roomease;
 
 /**
 *The Roomease class is the entry point for the application. It contains the main method
 *which creates and displays the login page of the application.
 */
-package roomease;
-
 public class Roomease {
     
     public static void main(String[] args) {
